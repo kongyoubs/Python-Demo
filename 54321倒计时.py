362ddc3f92d1cc3501b20651ed92f8e15abdba1e
kongyoubs <45224252+kongyoubs@users.noreply.github.com>
2020-04-19 16:08:47 +0800

    Create 54321倒计时.py

---
+++ 54321倒计时.py
@@ -0,0 +1,4 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> for countdown in 5, 4, 3, 2, 1, "hey!":
+	print(countdown)
